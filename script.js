77a7765dd04c491122912287aab8b515b6eca850
Eric Chan <ericchan024@gmail.com>
2023-02-24 17:57:51 -0500

    configured prettier

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 Book.prototype.isRead = function isRead() {
-    if(this.read === false) return "not read yet"
-    return "read already"
-}
+    if (this.read === false) return 'not read yet';
+    return 'read already';
+};
 
 Book.prototype.info = function info() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead()}`
-}
+    return `${this.title} by ${this.author}, ${
+        this.pages
+    } pages, ${this.isRead()}`;
+};
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 console.log(theHobbit.info());
