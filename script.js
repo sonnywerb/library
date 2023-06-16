899a4917df649572950442fcabc9a3017f894904
Eric Chan <ericchan024@gmail.com>
2023-06-15 21:30:59 -0400

    added delete book button and allow users to change read status

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+let table = document.querySelector('tbody');
+const form = document.querySelector('form');
 
 function Book(title, author, read = 'No') {
     this.title = title;
@@ -10,25 +12,73 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function deleteBook(book) {
+    const index = myLibrary.indexOf(book);
+    myLibrary.splice(index, 1);
+    table.innerHTML = '';
+    displayBooks();
+}
+
+function readBook(book, readCheckbox) {
+    const index = myLibrary.indexOf(book);
+    if (readCheckbox.checked) {
+        myLibrary[index].read = 'Yes';
+    } else {
+        myLibrary[index].read = 'No';
+    }
+    console.log(book);
+}
+
 function displayBooks() {
     myLibrary.forEach((book) => {
-        console.log(book);
+        let row = table.insertRow();
+        let titleCell = row.insertCell(0);
+        let authorCell = row.insertCell(1);
+        let readCell = row.insertCell(2);
+        let removeBook = row.insertCell(3);
+        row.dataset.index = myLibrary.indexOf(book);
+        let deleteBtn = createDeleteBtn();
+
+        let readCheckbox = createReadCheckbox(book);
+
+        titleCell.innerHTML = `${book.title}`;
+        authorCell.innerHTML = `${book.author}`;
+        readCell.appendChild(readCheckbox);
+        removeBook.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener('click', () => {
+            deleteBook();
+        });
+
+        readCheckbox.addEventListener('change', () => {
+            readBook(book, readCheckbox);
+        });
     });
 }
 
-const percyJackson = new Book('Percy Jackson', 'Rick Rioridan');
-const harryPotter = new Book('Harry Potter', 'J.K. Rowling');
-const catInTheHat = new Book('The Cat In The Hat', 'Dr. Seuss', 'Yes');
+function createDeleteBtn() {
+    let newDeleteBtn = document.createElement('input');
+    newDeleteBtn.type = 'button';
+    newDeleteBtn.className = 'deleteBtn';
+    newDeleteBtn.value = 'Delete';
+    return newDeleteBtn;
+}
 
-addBookToLibrary(percyJackson);
-addBookToLibrary(harryPotter);
-addBookToLibrary(catInTheHat);
-displayBooks();
+function createReadCheckbox(book) {
+    let newRedCheckbox = document.createElement('input');
+    newRedCheckbox.type = 'checkbox';
+    newRedCheckbox.name = 'read';
+    if (book.read === 'Yes') {
+        newRedCheckbox.checked = true;
+    }
+    return newRedCheckbox;
+}
 
 // button to open modal
 const openModal = document.querySelector('#open-modal');
 const modal = document.querySelector('#modal');
 const close = document.querySelector('.close');
+const submit = document.querySelector('#submit');
 
 openModal.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -36,4 +86,31 @@ openModal.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
     modal.style.display = 'none';
+});
+
+submit.addEventListener('click', (event) => {
+    const newTitle = document.getElementById('title').value;
+    const newAuthor = document.getElementById('author').value;
+    const read = document.querySelector('#read');
+    let newBook;
+
+    if (read.checked === true) {
+        newBook = new Book(`${newTitle}`, `${newAuthor}`, `Yes`);
+    } else {
+        newBook = new Book(`${newTitle}`, `${newAuthor}`);
+    }
+
+    // add book to array
+    addBookToLibrary(newBook);
+    console.log(myLibrary);
+
+    table.innerHTML = '';
+
+    displayBooks();
+
+    // reset form and close modal
+    form.reset();
+    modal.style.display = 'none';
+
+    event.preventDefault();
 });
